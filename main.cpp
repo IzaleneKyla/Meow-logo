57ddd45ceddc57b7f0ae104496544fbf242ec954
IzaleneKyla <izalenekyla07@gmail.com>
2025-09-21 16:38:32 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () { 
+
+    cout << "MMMM      MMMM EEEEEE OOOOOOO WW        WW        WW" << endl;
+    cout << "MM MM    MM MM EE     OO   OO  WW      WW WW     WW" << endl;
+    cout << "MM  MM  MM  MM EEEEEE OO   OO   WW    WW   WW   WW" << endl;
+    cout << "MM   MM MM  MM EE     OO   OO    WW  WW     WW WW" << endl;
+    cout << "MM    MM    MM EEEEEE OOOOOOO     WW         WW" << endl;
+
+    return 0;
+}
